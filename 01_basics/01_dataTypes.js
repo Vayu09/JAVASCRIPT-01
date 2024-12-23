840ce7521deb4f5c5a5348c987bbdd32da522e9d
Vayu Nandan Tripathi <aryanlte22@gmail.com>
2024-12-23 23:29:02 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //  treat all JS code As newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+console.log(3+3); // code readability should  be high
+
+console.log("vayu");
+
+let name = " vayu"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+
+//number => 2 to power 53
+// bigint (use to in big integers like fb and reddit)
+// string => ""
+// boolean => ture/false
+// null => standalone value 
+// undefind => 
+// symbol => unique
+
+//object
+
+console.log(typeof "vayu"); // to tell the type of data type we usr console.log(typeof----)
+console.log(typeof age);
+console.log(typeof false);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
+
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 1444553
+let accountEmail = "vayu@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 not allowed 
+
+
+
+
+accountEmail = "aryan2gmail.com"
+accountPassword = "123132"
+accountCity = "varanasi"
+ 
+/*
+prefer not use var 
+Because of issue in block scope and functional Scope
+*/
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
