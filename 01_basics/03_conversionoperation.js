89658962d75f253a4703aa768dded98be61bd181
Vayu Nandan Tripathi <aryanlte22@gmail.com>
2025-01-02 04:26:01 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -53,8 +53,8 @@ let str3 = str1 + str2
 // console.log(1 + 2 + "2");
 // console.log((3 + 4) * 5 % 3);
 
-console.log(+true);
-console.log(+"");
+//console.log(+true);
+//console.log(+"");
 
 let num1, num2, num3
 
@@ -62,7 +62,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 gameCounter++;
-console.log(gameCounter);
+//console.log(gameCounter);
 
 
 
